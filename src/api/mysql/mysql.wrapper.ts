bf500f7562b6d21d6bab3726f883c4f1d9d1e290
Diana0604 <diana.vallverdu@gmail.com>
2022-10-05 11:50:44 +0100

    fix format for mysql wrapper

--- src/api/mysql/mysql.wrapper.ts
+++ src/api/mysql/mysql.wrapper.ts
@@ -1,31 +1,31 @@
 //config
-import config from "../../config/config.index";
+import config from "../../config/config.index"
 //database
-import { Connection, createConnection } from "mysql";
+import { Connection, createConnection } from "mysql"
 import { createTableCommand, insertElementCommand } from './mysql.helpers'
 
-let connection: Connection;
+let connection: Connection
 
 /**
  * generates database connection to be used throughout the app, using configuration in src/config/config.index.ts
  */
 export const init = () => {
     try {
-        connection = createConnection(config.mysqlConfig);
-        console.debug('MySql created connection succesfully');
+        connection = createConnection(config.mysqlConfig)
+        console.debug('MySql created connection succesfully')
         return true
     } catch (error) {
-        console.error('[mysql.connector][init][Error]: ', error);
-        throw new Error('failed to initialize connection');
+        console.error('[mysql.connector][init][Error]: ', error)
+        throw new Error('failed to initialize connection')
     }
-};
+}
 
 /**
  * Disconnect from MySql
  */
 export const disconnect = () => {
     try {
-        connection.end();
+        connection.end()
     } catch (error) {
         console.log('MySQL error when trying to disconnect')
         console.log(error)
@@ -42,15 +42,15 @@ export const execute = <T>(query: string): Promise<T> => {
     try {
         return new Promise<T>((resolve, reject) => {
             connection.query(query, (error, results, _fields) => {
-                if (error) reject(error);
+                if (error) reject(error)
                 else {
                     resolve(results)
                 }
             })
         })
     } catch (error) {
-        console.error('[mysql.connector][execute][Error]: ', error);
-        throw new Error('failed to execute MySQL query');
+        console.error('[mysql.connector][execute][Error]: ', error)
+        throw new Error('failed to execute MySQL query')
     }
 }
 
