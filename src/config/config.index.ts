417edacfd28931e84d39ce98f359840eec3bc1d8
root <root@raspbx>
2022-11-28 23:56:12 +0000

    update config

--- src/config/config.index.ts
+++ src/config/config.index.ts
@@ -5,13 +5,14 @@ const config: IConfig = {
         connectionLimit: 5,
         user: "user", //default is sa
         password: "mypassword",
-        host: "localhost", // for local machine
+        host: "127.0.0.1", // for local machine
         database: "ictheatre", // name of database
+	//port: 3306,
         options: {
             encrypt: true
         }
     },
-    seedDB: true, //set to false if you don't want to seed the dab
+    seedDB: false, //set to false if you don't want to seed the dab
     showConfig: {
         lengthInSeconds: 300, //length of show in seconds
         updateIntervalInSeconds: 0.2 //how often do we update companies prices
