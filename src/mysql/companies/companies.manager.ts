5f80148afd6021959eb5426b7e7ebefe41a4265f
Diana0604 <diana.vallverdu@gmail.com>
2022-11-30 20:00:22 +0000

    tidy up companies manager

--- src/mysql/companies/companies.manager.ts
+++ src/mysql/companies/companies.manager.ts
@@ -17,7 +17,6 @@ import {
 import { ShareBundle } from "../../objects/ShareBundle";
 import { getSellersListFromDB } from "../sellers/sellers.manager";
 
-
 /**
  *
  * @returns list of all companies from database
@@ -34,33 +33,28 @@ export const getCompaniesListFromDB = async () => {
 };
 
 /**
- * Get information for player company
- * @returns player company object
- */
- export const getPlayerCompanyFromDB = async () => {
-  return await getFirstTableElement(PlayerCompany.name);
-};
-
-
-/**
  * Get information for just one company
  * @param companyId id of company you wish to retreive info for
  * @returns company object
  */
- export const getCompanyFromDB = async (companyId: string) => {
+export const getCompanyFromDB = async (companyId: string) => {
   return await getElementById(companyId, Company.name);
 };
 
-export const addCompanyToDB = async (company: ICompanyProperties) => {
-  await insertElement(new Company(company));
+/**
+ * Get information for player company
+ * @returns player company object
+ */
+export const getPlayerCompanyFromDB = async () => {
+  return await getFirstTableElement(PlayerCompany.name);
 };
 
-
-export const editPlayerCompanyInDB = async (
-  playerCompany: IPlayerCompanyProperties
-) => {
-  const newCompany = new PlayerCompany(playerCompany);
-  return await updateElement(newCompany);
+/**
+ * Create a new company and save it in database
+ * @param company
+ */
+export const addCompanyToDB = async (company: ICompanyProperties) => {
+  await insertElement(new Company(company));
 };
 
 /**
@@ -68,10 +62,20 @@ export const editPlayerCompanyInDB = async (
  * @param newCompany
  * @returns
  */
-export const editCompanyInDB = async (
-  newCompany: ICompanyProperties
-) => {
+export const editCompanyInDB = async (newCompany: ICompanyProperties) => {
   return await updateElement(new Company(newCompany));
+};
+
+/**
+ * Edit player company in database with new properties
+ * @param playerCompany
+ * @returns
+ */
+export const editPlayerCompanyInDB = async (
+  playerCompany: IPlayerCompanyProperties
+) => {
+  const newCompany = new PlayerCompany(playerCompany);
+  return await updateElement(newCompany);
 };
 
 /**
